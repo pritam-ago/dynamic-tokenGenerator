dbf26a2d471cc0611b82bd8051ccfe701ecfffb5
Vikas Pritam <87309275+pritam-ago@users.noreply.github.com>
2025-04-21 23:05:51 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { createConfig, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http } from "viem";
+import { mainnet } from "viem/chains";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
+const config = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <div className="App">
+    <DynamicContextProvider
+      settings={{
+        environmentId: "122f8d7f-3392-4563-8a04-98a64026b7fe",
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+            <DynamicWidget />
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
+    </div>
+    
+  );
+}
